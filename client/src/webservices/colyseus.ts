825a44a26ee8f5632f97e0a444f0715dfeaaf219
Thomas DA ROCHA <thomas.darocha@axopen.com>
2026-02-25 22:42:45 +0100

    feat(cors)

--- client/src/webservices/colyseus.ts
+++ client/src/webservices/colyseus.ts
@@ -1,4 +1,16 @@
-import { Client } from "@colyseus/sdk";
+import {Auth, Client} from '@colyseus/sdk';
+import {HTTP} from '@colyseus/sdk/HTTP';
 import { env } from "./env.ts";
 
-export const colyseusClient = new Client(env.colyseusUrl);
+export const colyseusClient = createColyseusClient();
+
+
+function createColyseusClient() {
+    const client = new Client(env.colyseusUrl);
+    client.http = new HTTP(client, {
+        headers: {},
+        credentials: 'omit'
+    });
+    client.auth = new Auth(client.http);
+    return client;
+}
