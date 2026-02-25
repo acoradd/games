8614195d82acacda31597871b3b45861906fa601
Thomas DA ROCHA <thomas.darocha@axopen.com>
2026-02-25 11:50:51 +0100

    chore(lib): remove unused code and libs

--- client/vite/config.prod.mjs
+++ client/vite/config.prod.mjs
@@ -1,37 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const phasermsg = () => {
-    return {
-        name: 'phasermsg',
-        buildStart() {
-            process.stdout.write(`Building for production...\n`);
-        },
-        buildEnd() {
-            const line = "---------------------------------------------------------";
-            const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
-            process.stdout.write(`${line}\n${msg}\n${line}\n`);
-
-            process.stdout.write(`✨ Done ✨\n`);
-        }
-    }
-}
-
 export default defineConfig({
     base: '/',
     plugins: [
-        react(),
-        phasermsg()
+        react()
     ],
     logLevel: 'warning',
     build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    phaser: ['phaser']
-                }
-            }
-        },
         minify: 'terser',
         terserOptions: {
             compress: {
